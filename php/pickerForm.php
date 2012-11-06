70c3fc59dd637255de5fdc716a98b8e6eda41e86
arkhipus <alexar@gmail.com>
2012-11-06 01:37:39 -0800

    Added mapXml.php

--- php/pickerForm.php
+++ php/pickerForm.php
@@ -3,16 +3,51 @@
 ?>
 <script language="JavaScript">
     $(document).ready(function() {
+        initialize();
+        
         $('#search').click(function() {
-            alert($('#zip').val() + ":" + $('#dist').val());
+            //alert($('#address').val() + ":" + $('#dist').val());
+            codeAddress();
         });
     });
+    
+    var geocoder;
+    //var map;
+   
+    function initialize() {
+        geocoder = new google.maps.Geocoder();
+        var latlng = new google.maps.LatLng(-34.397, 150.644);
+        var mapOptions = {
+            zoom: 8,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        //map = new google.maps.Map($('#map_canvas'), mapOptions);
+    }
+
+    function codeAddress() {
+        var address = $('#address').val();
+        geocoder.geocode( { 'address': address}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                //map.setCenter(results[0].geometry.location);
+                
+                alert(results[0].geometry.location);
+                
+                //var marker = new google.maps.Marker({
+                //    map: map,
+                //    position: results[0].geometry.location
+                //});
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
+    }
 </script>
 <form>
     <div id="container">
         <div>
-            <label for="zip"><span>Your ZIP code:</span></label>
-            <input name="zip" type="text" id="zip" />
+            <label for="address"><span>Address:</span></label>
+            <input name="address" type="text" id="address" />
         </div>
         <div>
             <label for="dist"><span>Distance in miles:</span></label>
@@ -29,5 +64,6 @@
         <div>
             <input type="button" value="Search" id="search" name="search" />
         </div>
+        <div id="msg" name="msg"></div>
     </div>
 </form>
