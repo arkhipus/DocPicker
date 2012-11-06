ac24dbef006a44f09f5f015e496b5f4c75c9fc8e
arkhipus <alexar@gmail.com>
2012-11-06 01:37:00 -0800

    Added mapXml.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Doctor Finder</title>
         <link rel="stylesheet" type="text/css" href="css/main.css">
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
     </head>
     <body>
